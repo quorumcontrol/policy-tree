4f16c606d9d8130b8d8594cb2cf27891d3ab4f27
Topper Bowers <topper@toppingdesign.com>
2020-08-24 09:25:43 +0200

    working in typescript

--- packages/lockdown/src/index.spec.ts
+++ packages/lockdown/src/index.spec.ts
@@ -1,15 +1,32 @@
 import 'mocha'
 import { expect } from 'chai'
 import { Sandbox } from './index'
+import 'ses'
 
+const code = `
+function sleep(time) {
+    return new Promise((resolve)=> {
+        setTimeout(resolve, time);
+    })
+}
+
+async function run() {
+    await sleep(200)
+    print('hi')
+    return 'hi'
+}
+run()
+`
 
 describe("sanity", ()=> {
     it('runs', async ()=> {
-        const sandbox = new Sandbox({
-            print: harden(console.log),
+        const sandbox = new Sandbox(code, {
+            endowments: {
+                setTimeout: setTimeout, 
+                print: harden(console.log),
+            },
         })
-        sandbox.evaluate(`
-            print('hihi')
-        `)
+        const result = await sandbox.evaluate()
+        expect(result).to.equal('hi')
     })
 })
--- packages/lockdown/src/index.ts
+++ packages/lockdown/src/index.ts
@@ -1,4 +1,6 @@
 import 'ses'
+import { makeMeteringTransformer, makeMeter } from '@agoric/transform-metering/dist/transform-metering.cjs';
+import * as babelCore from '@babel/core';
 
 declare const lockdown:any;
 declare class Compartment {
@@ -6,16 +8,60 @@ declare class Compartment {
     evaluate(code:string):any;
 }
 
+const transformer = makeMeteringTransformer(babelCore)
+
+lockdown({ errorTaming: 'unsafe' })
+
+// see https://github.com/Agoric/agoric-sdk/blob/f755266d57c9358cd1c247eebd57f574ba892b01/packages/transform-metering/src/constants.js
+interface SandboxOpts {
+    budgets?: {
+        budgetCombined?: number,
+        budgetAllocate?: boolean,
+        budgetCompute?: boolean,
+        budgetStack?: number,
+    },
+    endowments?: {[key:string]:any}
+}
+
+const defaultBudgets = {
+    budgetCombined: 10000,    
+}
+
+const defaultEndowments = {}
+
+const defaultSandboxOpts = {
+    budgets: defaultBudgets,
+    endowments: defaultEndowments,
+}
+
 export class Sandbox {
     compartment:Compartment
+    meter: any
+    refillFacet: any
+    transformed: any
 
-    constructor(endowment:any) {
-        this.compartment = new Compartment(endowment)
+    constructor(code:string, opts:SandboxOpts=defaultSandboxOpts) {
+        const budgets = {...defaultBudgets, ...opts.budgets}
+        const endowments = {...defaultEndowments, ...opts.endowments}
+
+        const { meter, refillFacet } = makeMeter(budgets);
+        this.refillFacet = refillFacet
+        const hardenedMeter = harden(meter)
+        this.meter = hardenedMeter
+        const getMeter = ()=> hardenedMeter
+
+        const transformed = transformer.rewrite({
+            src: code,
+            endowments: { getMeter },
+            sourceType: 'script',
+        })
+        this.transformed = transformed
+
+        this.compartment = new Compartment({...endowments, ...transformed.endowments})
     }
 
-    evaluate(code:string) {
-        return this.compartment.evaluate(code)
+    evaluate() {
+        return this.compartment.evaluate(this.transformed.src)
     }
 }
 
-lockdown()
